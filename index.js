37859200951735fef304e060abbdd4f1b6dac3a6
Full Stack Software Engineer <darkinstar@outlook.com>
2023-11-23 16:13:13 -0500

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const schedule = require('node-schedule'); 
 const shell = require('shelljs');
 const { IncomingWebhook } = require('@slack/webhook')
+const moment = require('moment')
 
 const webHookUrl = process.env.SLACK_WEBHOOK ?? 'https://hooks.slack.com/services/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'
 const environment = process.env.APP_ENV ?? 'development'
@@ -17,9 +18,11 @@ const job = schedule.scheduleJob('*/5 * * * * *', async () => {
     if(status !== 'active') { 
         await shell.exec(`echo ${password} | sudo -S systemctl start nginx`, {silent: true})
         
+        const dateTime = moment().format('l')
+
         const message = {
             channel: '#server-alerts',
-            text: `<${serverUrl}|${serverName} is restarted>`,
+            text: `<${serverUrl}|${serverName} is restarted> at ${dateTime}`,
             username: environment,
             icon_emoji: ':makeitrain:',
             unfurl_links: true,

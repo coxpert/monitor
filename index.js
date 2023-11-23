d02f60530b02e84662d3b8039bc786fdfa9fe970
Full Stack Software Engineer <darkinstar@outlook.com>
2023-11-23 13:13:38 -0500

    commit

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const schedule = require('node-schedule');
-const { exec } = require('child_process');
+require('dotenv').config()
+const schedule = require('node-schedule'); 
+const shell = require('shelljs');
+const { IncomingWebhook } = require('@slack/webhook')
 
-const job = schedule.scheduleJob('*/3 * * * * *', function(){
-    exec('node -v', (err, stdout, stderr) => {
-        if (err) {
-          return;
-        }
-       
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
-      });
+const webHookUrl = process.env.SLACK_WEBHOOK ?? 'https://hooks.slack.com/services/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'
+const environment = process.env.APP_ENV ?? 'development'
+const serverUrl = process.env.APP_SERVER ?? 'https://forge.laravel.com/servers/719367'
+
+const webhookClient = new IncomingWebhook(webHookUrl);
+
+const job = schedule.scheduleJob('*/5 * * * * *', async () => {
+    const response = await shell.exec('systemctl is-active nginx')
+    const status = response.stdout.trim();
+    if(status !== 'active') {
+        await shell.exec('systemctl start nginx')
+        
+        const message = {
+            channel: '#server-alerts',
+            text: `<${serverUrl}|Server is restarted>`,
+            username: environment,
+            icon_emoji: ':makeitrain:',
+            unfurl_links: true,
+            unfurl_media: false,
+            link_names: true,
+        };
+
+        await webhookClient.send(message);
+    }
 });
